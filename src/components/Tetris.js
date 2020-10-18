7bb1d32558ab80301775004d03a4b6a79f031230
Aurelien Gaillard <fakeemail@gadzo.org>
2020-10-18 16:16:32 +0200

    adding tetrominos

---
+++ src/components/Tetris.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import Stage from './Stage'
+import Display from './Display'
+import StartButton from './StartButton'
+
+import { createStage } from '../gameHelpers'
+
+
+const Tetris = () => {
+
+    return (
+        <div>
+            <Stage stage={createStage()} />
+            <aside>
+                <div>
+                    <Display text="SScore" />
+                    <Display text="Rows" />
+                    <Display text="Level" />
+                </div>
+                <StartButton />
+            </aside>
+        </div>
+    )
+}
+
+export default Tetris
